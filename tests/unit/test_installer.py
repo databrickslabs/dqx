f46269cf8666fd9bec44a38ce80afdd42631ca22
Marcin Wojtyczka <marcin.wojtyczka@databricks.com>
2025-01-03 13:07:29 +0100

    updated tests

--- tests/unit/test_installer.py
+++ tests/unit/test_installer.py
@@ -1,10 +1,11 @@
-from unittest.mock import patch, MagicMock, create_autospec
+from unittest.mock import patch, MagicMock
 import pytest
 from databricks.labs.dqx.installer.install import WorkspaceInstaller, ManyError
+from databricks.sdk import WorkspaceClient
 
 
 def test_installer_executed_outside_workspace():
-    mock_ws_client = MagicMock()
+    mock_ws_client = MagicMock(spec=WorkspaceClient)
     with pytest.raises(SystemExit) as exc_info:
         WorkspaceInstaller(mock_ws_client, environ={"DATABRICKS_RUNTIME_VERSION": "7.3"})
     assert str(exc_info.value) == "WorkspaceInstaller is not supposed to be executed in Databricks Runtime"
@@ -12,7 +13,7 @@ def test_installer_executed_outside_workspace():
 
 def test_configure_raises_timeout_error():
     mock_configure = MagicMock(side_effect=TimeoutError("Mocked timeout error"))
-    mock_ws_client = MagicMock()
+    mock_ws_client = MagicMock(spec=WorkspaceClient)
     installer = WorkspaceInstaller(mock_ws_client)
 
     with patch.object(installer, 'configure', mock_configure):
@@ -25,7 +26,7 @@ def test_configure_raises_timeout_error():
 def test_configure_raises_single_error():
     single_error = ValueError("Single error")
     mock_configure = MagicMock(side_effect=ManyError([single_error]))
-    mock_ws_client = MagicMock()
+    mock_ws_client = MagicMock(spec=WorkspaceClient)
     installer = WorkspaceInstaller(mock_ws_client)
 
     with patch.object(installer, 'configure', mock_configure):
@@ -40,7 +41,7 @@ def test_configure_raises_many_errors():
     second_error = ValueError("Second error")
     errors = [first_error, second_error]
     mock_configure = MagicMock(side_effect=ManyError(errors))
-    mock_ws_client = MagicMock()
+    mock_ws_client = MagicMock(spec=WorkspaceClient)
     installer = WorkspaceInstaller(mock_ws_client)
 
     with patch.object(installer, 'configure', mock_configure):
