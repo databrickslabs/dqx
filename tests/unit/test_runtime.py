e299f2a6ba82e84c31c52cc9154647f823a35e1e
Marcin Wojtyczka <marcin.wojtyczka@databricks.com>
2025-01-03 12:01:55 +0100

    added tests

--- tests/unit/test_runtime.py
+++ tests/unit/test_runtime.py
@@ -1,3 +1,4 @@
+import sys
 import pytest
 from databricks.labs.dqx.runtime import main
 
@@ -15,3 +16,9 @@ def test_runtime_no_config():
 def test_runtime_missing_config():
     with pytest.raises(FileNotFoundError, match='config_path'):
         main("--workflow=profiler", "--config=config_path")
+
+
+def test_runtime_args_provided_as_sys_args():
+    with pytest.raises(FileNotFoundError, match='config_path'):
+        sys.argv = [__file__, "--workflow=profiler", "--config=config_path"]
+        main()
