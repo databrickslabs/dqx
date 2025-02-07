1ce4fe065b539cd8b92480055db26175117c3165
Marcin Wojtyczka <marcin.wojtyczka@databricks.com>
2025-02-07 16:29:33 +0100

    updated docs added apply checks test

--- tests/unit/test_apply_checks.py
+++ tests/unit/test_apply_checks.py
@@ -9,24 +9,20 @@ from databricks.sdk import WorkspaceClient
 
 def test_apply_checks(spark_local):
     ws = MagicMock(spec=WorkspaceClient, **{"catalogs.list.return_value": []})
-    dq_engine = DQEngine(ws)
 
-    schema = "a: int"
-    test_df = spark_local.createDataFrame([[1], [None]], schema)
+    schema = "a: int, b: int, c: int"
+    expected_schema = schema + ", _errors: map<string,string>, _warnings: map<string,string>"
+    test_df = spark_local.createDataFrame([[1, None, 3]], schema)
 
     checks = [
-        DQRule(name="col_a_is_null_or_empty", criticality="error", check=is_not_null_and_not_empty("a")),
+        DQRule(name="col_a_is_null_or_empty", criticality="warn", check=is_not_null_and_not_empty("a")),
+        DQRule(name="col_b_is_null_or_empty", criticality="error", check=is_not_null_and_not_empty("b")),
     ]
 
-    checked = dq_engine.apply_checks(test_df, checks)
+    dq_engine = DQEngine(ws)
+    df = dq_engine.apply_checks(test_df, checks)
 
-    expected_schema = schema + ", _errors: map<string,string>, _warnings: map<string,string>"
-    expected = spark_local.createDataFrame(
-        [
-            [1, None, None],
-            [None, {"col_a_is_null_or_empty": "Column a is null or empty"}, None],
-        ],
-        expected_schema,
+    expected_df = spark_local.createDataFrame(
+        [[1, None, 3, {"col_b_is_null_or_empty": "Column b is null or empty"}, None]], expected_schema
     )
-
-    assert_df_equality(checked, expected, ignore_nullable=True)
+    assert_df_equality(df, expected_df)
