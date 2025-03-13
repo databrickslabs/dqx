f8ef516e49a752d5ca66fae6e8e4e6005142f5f5
Greg Hansen <gregory.hansen@databricks.com>
2025-03-13 12:51:47 -0400

    Add tests

--- tests/integration/test_col_functions.py
+++ tests/integration/test_col_functions.py
@@ -270,7 +270,7 @@ def test_is_col_older_than_col2_for_n_days(spark):
 
     actual = test_df.select(
         is_older_than_col2_for_n_days("a", "b", 2),
-        is_older_than_col2_for_n_days(F.col("c").getItem("val"), F.try_element_at("d", F.lit(1))),
+        is_older_than_col2_for_n_days(F.col("c").getItem("val"), F.try_element_at("d", F.lit(1)), 2),
     )
 
     checked_schema = (
@@ -384,7 +384,7 @@ def test_col_is_not_in_near_future(spark):
         is_not_in_near_future(F.col("c").getItem("dt"), 2, F.lit("2023-01-10 11:08:40")),
     )
 
-    checked_schema = "a_in_near_future: string, cast_b_as_timestamp_in_near_future: string"
+    checked_schema = "a_in_near_future: string, cast_b_as_timestamp_in_near_future: string, unresolvedextractvalue_c_dt_in_near_future: string"
     expected = spark.createDataFrame(
         [
             [None, None, None],
@@ -472,7 +472,7 @@ def test_col_is_not_less_than(spark, set_utc_timezone):
                 None,
                 None,
                 "Value 1.99 is less than limit: 2",
-                "Value 1 is less than limit: 2",
+                None,
             ],
             [None, "Value 4 is less than limit: 6", "Value 3 is less than limit: 4", None, None, None, None],
             [None, None, None, None, None, None, None],
@@ -508,7 +508,7 @@ def test_col_is_not_greater_than(spark, set_utc_timezone):
     checked_schema = (
         "a_greater_than_limit: string, a_greater_than_limit: string, b_greater_than_limit: string, "
         "c_greater_than_limit: string, d_greater_than_limit: string, e_greater_than_limit: string, "
-        "try_element_at_greater_than_limit: string"
+        "try_element_at_f_1_greater_than_limit: string"
     )
     expected = spark.createDataFrame(
         [
@@ -540,15 +540,15 @@ def test_col_is_not_greater_than(spark, set_utc_timezone):
 
 
 def test_col_is_in_range(spark, set_utc_timezone):
-    schema_num = "a: int, b: date, c: timestamp, d: int, e: int, f: int, g: decimal(10,2)"
+    schema_num = "a: int, b: date, c: timestamp, d: int, e: int, f: int, g: decimal(10,2), h: map<string, int>"
     test_df = spark.createDataFrame(
         [
-            [0, datetime(2024, 12, 1).date(), datetime(2024, 12, 1), -1, 5, 6, Decimal("2.00")],
-            [1, datetime(2025, 1, 1).date(), datetime(2025, 1, 1), 2, 6, 3, Decimal("1.00")],
-            [2, datetime(2025, 2, 1).date(), datetime(2025, 2, 1), 2, 7, 3, Decimal("3.00")],
-            [3, datetime(2025, 3, 1).date(), datetime(2025, 3, 1), 3, 8, 3, Decimal("1.01")],
-            [4, datetime(2025, 4, 1).date(), datetime(2025, 4, 1), 2, 9, 3, Decimal("3.01")],
-            [None, None, None, None, None, None, None],
+            [0, datetime(2024, 12, 1).date(), datetime(2024, 12, 1), -1, 5, 6, Decimal("2.00"), {"val": 0}],
+            [1, datetime(2025, 1, 1).date(), datetime(2025, 1, 1), 2, 6, 3, Decimal("1.00"), {"val": 1}],
+            [2, datetime(2025, 2, 1).date(), datetime(2025, 2, 1), 2, 7, 3, Decimal("3.00"), {"val": 2}],
+            [3, datetime(2025, 3, 1).date(), datetime(2025, 3, 1), 3, 8, 3, Decimal("1.01"), {"val": 3}],
+            [4, datetime(2025, 4, 1).date(), datetime(2025, 4, 1), 2, 9, 3, Decimal("3.01"), {"val": 4}],
+            [None, None, None, None, None, None, None, {"val": None}],
         ],
         schema_num,
     )
@@ -562,11 +562,13 @@ def test_col_is_in_range(spark, set_utc_timezone):
         is_in_range("d", F.col("a"), F.expr("e - 1")),
         is_in_range("f", "a", 5),
         is_in_range("g", 1, 3),
+        is_in_range(F.col("h").getItem("val"), 1, 3),
     )
 
     checked_schema = (
         "a_not_in_range: string, b_not_in_range: string, c_not_in_range: string, "
-        "d_not_in_range: string, f_not_in_range: string, g_not_in_range: string"
+        "d_not_in_range: string, f_not_in_range: string, g_not_in_range: string "
+        "unresolvedextractvalue_h_val_not_in_range: string"
     )
     expected = spark.createDataFrame(
         [
@@ -577,10 +579,11 @@ def test_col_is_in_range(spark, set_utc_timezone):
                 "Value -1 not in range: [ 0 , 4 ]",
                 "Value 6 not in range: [ 0 , 5 ]",
                 None,
+                "Value 0 not in range: [ 1 , 3 ]",
             ],
-            [None, None, None, None, None, None],
-            [None, None, None, None, None, None],
-            [None, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
             [
                 "Value 4 not in range: [ 1 , 3 ]",
                 "Value 2025-04-01 not in range: [ 2025-01-01 , 2025-03-01 ]",
@@ -588,8 +591,9 @@ def test_col_is_in_range(spark, set_utc_timezone):
                 "Value 2 not in range: [ 4 , 8 ]",
                 "Value 3 not in range: [ 4 , 5 ]",
                 "Value 3.01 not in range: [ 1 , 3 ]",
+                "Value 4 not in range: [ 1 , 3 ]",
             ],
-            [None, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
         ],
         checked_schema,
     )
@@ -598,13 +602,13 @@ def test_col_is_in_range(spark, set_utc_timezone):
 
 
 def test_col_is_not_in_range(spark, set_utc_timezone):
-    schema_num = "a: int, b: date, c: timestamp, d: timestamp, e: decimal(10,2)"
+    schema_num = "a: int, b: date, c: timestamp, d: timestamp, e: decimal(10,2), f: array<int>"
     test_df = spark.createDataFrame(
         [
-            [0, datetime(2024, 12, 31).date(), datetime(2025, 1, 4), datetime(2025, 1, 7), Decimal("0.99")],
-            [1, datetime(2025, 1, 1).date(), datetime(2025, 1, 3), datetime(2025, 1, 1), Decimal("1.00")],
-            [3, datetime(2025, 2, 1).date(), datetime(2025, 2, 1), datetime(2025, 2, 3), Decimal("3.00")],
-            [None, None, None, None, None],
+            [0, datetime(2024, 12, 31).date(), datetime(2025, 1, 4), datetime(2025, 1, 7), Decimal("0.99"), [0, 1]],
+            [1, datetime(2025, 1, 1).date(), datetime(2025, 1, 3), datetime(2025, 1, 1), Decimal("1.00"), [1, 2]],
+            [3, datetime(2025, 2, 1).date(), datetime(2025, 2, 1), datetime(2025, 2, 3), Decimal("3.00"), [3, 4]],
+            [None, None, None, None, None, [None, 1]],
         ],
         schema_num,
     )
@@ -617,14 +621,16 @@ def test_col_is_not_in_range(spark, set_utc_timezone):
         is_not_in_range("c", start_date, end_date),
         is_not_in_range("d", "c", F.expr("cast(b as timestamp) + INTERVAL 2 DAY")),
         is_not_in_range("e", 1, 3),
+        is_not_in_range(F.try_element_at("f", F.lit(1)), 1, 3),
     )
 
     checked_schema = (
-        "a_in_range: string, b_in_range: string, c_in_range: string, d_in_range: string, e_in_range: string"
+        "a_in_range: string, b_in_range: string, c_in_range: string, d_in_range: string, e_in_range: string "
+        "try_element_at_f_1_in_range: string"
     )
     expected = spark.createDataFrame(
         [
-            [None, None, None, None, None],
+            [None, None, None, None, None, None],
             [
                 "Value 1 in range: [ 1 , 3 ]",
                 "Value 2025-01-01 in range: [ 2025-01-01 , 2025-01-03 ]",
@@ -638,8 +644,9 @@ def test_col_is_not_in_range(spark, set_utc_timezone):
                 None,
                 "Value 2025-02-03 00:00:00 in range: [ 2025-02-01 00:00:00 , 2025-02-03 00:00:00 ]",
                 "Value 3.00 in range: [ 1 , 3 ]",
+                "Value 3 in range: [ 1 , 3 ]",
             ],
-            [None, None, None, None, None],
+            [None, None, None, None, None, None],
         ],
         checked_schema,
     )
@@ -648,17 +655,29 @@ def test_col_is_not_in_range(spark, set_utc_timezone):
 
 
 def test_col_matching_regex(spark):
-    schema_str = "a: string"
-    test_df = spark.createDataFrame([["2023-01-02"], ["2023/01/02"], [None]], schema_str)
+    schema_str = "a: string, b: map<string, string>"
+    test_df = spark.createDataFrame(
+        [["2023-01-02", {"s": "2023-01-02"}], ["2023/01/02", {"s": "2023/01/02"}], [None, None, {"s": None}]],
+        schema_str,
+    )
 
     # matching ISO date: yyyy-MM-dd format
     date_re = "^\\d{4}-([0]\\d|1[0-2])-([0-2]\\d|3[01])$"
 
-    actual = test_df.select(regex_match("a", date_re), regex_match("a", date_re, negate=True))
+    actual = test_df.select(
+        regex_match("a", date_re), regex_match("a", date_re, negate=True), regex_match(F.col("b").getItem("s"), date_re)
+    )
 
-    checked_schema = "a_not_matching_regex: string, a_matching_regex: string"
+    checked_schema = (
+        "a_not_matching_regex: string, a_matching_regex: string, unresolvedextractvalue_b_s_not_matching_regex: string"
+    )
     expected = spark.createDataFrame(
-        [[None, "Column a is matching regex"], ["Column a is not matching regex", None], [None, None]], checked_schema
+        [
+            [None, "Column a is matching regex", None],
+            ["Column a is not matching regex", None, "Column unresolvedextractvalue_b_s is not matching regex"],
+            [None, None, None],
+        ],
+        checked_schema,
     )
 
     assert_df_equality(actual, expected, ignore_nullable=True)
@@ -708,7 +727,8 @@ def test_col_is_not_in_near_future_cur(spark):
 def test_col_is_not_null_and_not_empty_array(spark):
     schema_array = (
         "str_col: array<string>, int_col: array<int> , timestamp_col: array<timestamp>, "
-        "date_col: array<string>, struct_col: array<struct<a: string, b: int>>"
+        "date_col: array<string>, struct_col: array<struct<a: string, b: int>>, "
+        "nested_array_col: map<string, array<string>>"
     )
     data = [
         (
@@ -717,15 +737,17 @@ def test_col_is_not_null_and_not_empty_array(spark):
             [None, datetime.strptime("2025-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")],
             [datetime.strptime("2025-01-01", "%Y-%m-%d"), None],
             [{"a": "x", "b": 1}, None],
+            {"arr": ["a", "b", None]},
         ),
-        ([], [], [], [], []),
-        (None, None, None, None, None),
+        ([], [], [], [], [], {"arr": []}),
+        (None, None, None, None, None, {"arr": None}),
         (
             ["non-empty"],
             [10],
             [datetime.strptime("2025-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")],
             [datetime.strptime("2025-01-01", "%Y-%m-%d")],
             [{"a": "y", "b": 2}],
+            {"arr": ["non-empty"]},
         ),
     ]
 
@@ -737,22 +759,25 @@ def test_col_is_not_null_and_not_empty_array(spark):
         is_not_null_and_not_empty_array("timestamp_col"),
         is_not_null_and_not_empty_array("date_col"),
         is_not_null_and_not_empty_array("struct_col"),
+        is_not_null_and_not_empty_array(F.col("nested_array_col").getItem("arr")),
     )
 
     checked_schema = (
         "str_col_is_null_or_empty_array: string, int_col_is_null_or_empty_array: string, "
         "timestamp_col_is_null_or_empty_array: string, date_col_is_null_or_empty_array: string, "
-        "struct_col_is_null_or_empty_array: string"
+        "struct_col_is_null_or_empty_array: string, "
+        "unresolvedextractvalue_nested_array_col_arr_is_null_or_empty_array: string"
     )
     # Create the data
     checked_data = [
-        (None, None, None, None, None),
+        (None, None, None, None, None, None),
         (
             "Column str_col is null or empty array",
             "Column int_col is null or empty array",
             "Column timestamp_col is null or empty array",
             "Column date_col is null or empty array",
             "Column struct_col is null or empty array",
+            "Column unresolvedextractvalue_nested_array_col_arr is null or empty array",
         ),
         (
             "Column str_col is null or empty array",
@@ -760,8 +785,9 @@ def test_col_is_not_null_and_not_empty_array(spark):
             "Column timestamp_col is null or empty array",
             "Column date_col is null or empty array",
             "Column struct_col is null or empty array",
+            "Column unresolvedextractvalue_nested_array_col_arr is null or empty array",
         ),
-        (None, None, None, None, None),
+        (None, None, None, None, None, None),
     ]
     expected = spark.createDataFrame(checked_data, checked_schema)
 
@@ -769,38 +795,45 @@ def test_col_is_not_null_and_not_empty_array(spark):
 
 
 def test_col_is_valid_date(spark, set_utc_timezone):
-    schema_array = "a: string, b: string, c: string, d: string"
+    schema_array = "a: string, b: string, c: string, d: string, e: map<string, string>"
     data = [
-        ["2024-01-01", "12/31/2025", "invalid_date", None],
-        ["12/31/2025", "2024-01-01", "invalid_date", None],
-        ["12/31/2025", "invalid_date", "2024-01-01", None],
+        ["2024-01-01", "12/31/2025", "invalid_date", None, {"dt": "2024-01-01"}],
+        ["12/31/2025", "2024-01-01", "invalid_date", None, {"dt": "12/31/2025"}],
+        ["12/31/2025", "invalid_date", "2024-01-01", None, {"dt": "12/31/2025"}],
     ]
 
     test_df = spark.createDataFrame(data, schema_array)
 
     actual = test_df.select(
-        is_valid_date("a"), is_valid_date("b", "MM/dd/yyyy"), is_valid_date("c", "yyyy-MM-dd"), is_valid_date("d")
+        is_valid_date("a"),
+        is_valid_date("b", "MM/dd/yyyy"),
+        is_valid_date("c", "yyyy-MM-dd"),
+        is_valid_date("d"),
+        is_valid_date(F.col("e").getItem("dt")),
     )
 
     checked_schema = """
         a_is_not_valid_date: string, 
         b_is_not_valid_date: string, 
         c_is_not_valid_date: string, 
-        d_is_not_valid_date: string
+        d_is_not_valid_date: string,
+        unresolvedextractvalue_e_dt_is_not_valid_date: string
         """
     checked_data = [
-        [None, None, "Value 'invalid_date' is not a valid date with format 'yyyy-MM-dd'", None],
+        [None, None, "Value 'invalid_date' is not a valid date with format 'yyyy-MM-dd'", None, None],
         [
             "Value '12/31/2025' is not a valid date",
             "Value '2024-01-01' is not a valid date with format 'MM/dd/yyyy'",
             "Value 'invalid_date' is not a valid date with format 'yyyy-MM-dd'",
             None,
+            "Value '12/31/2025' is not a valid date",
         ],
         [
             "Value '12/31/2025' is not a valid date",
             "Value 'invalid_date' is not a valid date with format 'MM/dd/yyyy'",
             None,
             None,
+            "Value '12/31/2025' is not a valid date",
         ],
     ]
     expected = spark.createDataFrame(checked_data, checked_schema)
@@ -809,11 +842,32 @@ def test_col_is_valid_date(spark, set_utc_timezone):
 
 
 def test_col_is_valid_timestamp(spark, set_utc_timezone):
-    schema_array = "a: string, b: string, c: string, d: string, e: string"
+    schema_array = "a: string, b: string, c: string, d: string, e: string, f: map<string, string>"
     data = [
-        ["2024-01-01 00:00:00", "12/31/2025 00:00:00", "invalid_timestamp", None, "2025-01-31T00:00:00"],
-        ["12/31/2025 00:00:00", "2024-01-01 00:00:00", "invalid_timestamp", None, "2025-01-31 00:00:00"],
-        ["2024-01-01T00:00:00", "invalid_timestamp", "2024-01-01 00:00:00", None, "1/31/2025 00:00:00"],
+        [
+            "2024-01-01 00:00:00",
+            "12/31/2025 00:00:00",
+            "invalid_timestamp",
+            None,
+            "2025-01-31T00:00:00",
+            {"dt": "2024-01-01 00:00:00"},
+        ],
+        [
+            "12/31/2025 00:00:00",
+            "2024-01-01 00:00:00",
+            "invalid_timestamp",
+            None,
+            "2025-01-31 00:00:00",
+            {"dt": "12/31/2025 00:00:00"},
+        ],
+        [
+            "2024-01-01T00:00:00",
+            "invalid_timestamp",
+            "2024-01-01 00:00:00",
+            None,
+            "1/31/2025 00:00:00",
+            {"dt": "2024-01-01 00:00:00"},
+        ],
     ]
 
     test_df = spark.createDataFrame(data, schema_array)
@@ -824,6 +878,7 @@ def test_col_is_valid_timestamp(spark, set_utc_timezone):
         is_valid_timestamp("c", "yyyy-MM-dd HH:mm:ss"),
         is_valid_timestamp("d"),
         is_valid_timestamp("e", "yyyy-MM-dd'T'HH:mm:ss"),
+        is_valid_timestamp(F.col("f").getItem("dt")),
     )
 
     checked_schema = """
@@ -831,13 +886,15 @@ def test_col_is_valid_timestamp(spark, set_utc_timezone):
         b_is_not_valid_timestamp: string, 
         c_is_not_valid_timestamp: string, 
         d_is_not_valid_timestamp: string,
-        e_is_not_valid_timestamp: string
+        e_is_not_valid_timestamp: string,
+        unresolvedextractvalue_f_dt_is_not_valid_timestamp: string
         """
     checked_data = [
         [
             None,
             None,
             "Value 'invalid_timestamp' is not a valid timestamp with format 'yyyy-MM-dd HH:mm:ss'",
+            None,
             None,
             None,
         ],
@@ -847,6 +904,7 @@ def test_col_is_valid_timestamp(spark, set_utc_timezone):
             "Value 'invalid_timestamp' is not a valid timestamp with format 'yyyy-MM-dd HH:mm:ss'",
             None,
             "Value '2025-01-31 00:00:00' is not a valid timestamp with format 'yyyy-MM-dd'T'HH:mm:ss'",
+            "Value '12/31/2025 00:00:00' is not a valid timestamp",
         ],
         [
             None,
@@ -854,6 +912,7 @@ def test_col_is_valid_timestamp(spark, set_utc_timezone):
             None,
             None,
             "Value '1/31/2025 00:00:00' is not a valid timestamp with format 'yyyy-MM-dd'T'HH:mm:ss'",
+            None,
         ],
     ]
     expected = spark.createDataFrame(checked_data, checked_schema)
