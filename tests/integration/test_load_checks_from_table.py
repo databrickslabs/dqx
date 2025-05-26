313232f2206e891887dc1efa1662d3da98c8c1dc
Greg Hansen <gregory.hansen@databricks.com>
2025-05-26 13:11:28 -0400

    Add run_config_name to save and load methods

--- tests/integration/test_load_checks_from_table.py
+++ tests/integration/test_load_checks_from_table.py
@@ -37,4 +37,37 @@ def test_load_checks_from_table(installation_ctx, make_schema, make_random, spar
     engine = DQEngine(client)
     engine.save_checks_in_table(TEST_CHECKS, table_name)
     checks = engine.load_checks_from_table(table_name, spark)
-    assert checks == TEST_CHECKS, "Checks were not loaded correctly"
+    assert checks == TEST_CHECKS, "Checks were not loaded correctly."
+
+
+def test_load_checks_from_table_with_run_config(installation_ctx, make_schema, make_random, spark):
+    client = installation_ctx.workspace_client
+    catalog_name = "main"
+    schema_name = make_schema(catalog_name=catalog_name).name
+    table_name = f"{catalog_name}.{schema_name}.{make_random(6).lower()}"
+
+    engine = DQEngine(client)
+    run_config_name = "workflow_001"
+    engine.save_checks_in_table(TEST_CHECKS[0], table_name, run_config_name=run_config_name)
+    checks = engine.load_checks_from_table(table_name, run_config_name=run_config_name, spark=spark)
+    assert checks == TEST_CHECKS[0], "Checks were not loaded correctly for workflow run config."
+
+    engine.save_checks_in_table(TEST_CHECKS[1:], table_name)
+    checks = engine.load_checks_from_table(table_name, spark=spark)
+    assert checks == TEST_CHECKS[1:], "Checks were not loaded correctly for default run config."
+
+
+def test_save_checks_to_table_output_modes(installation_ctx, make_schema, make_random, spark):
+    client = installation_ctx.workspace_client
+    catalog_name = "main"
+    schema_name = make_schema(catalog_name=catalog_name).name
+    table_name = f"{catalog_name}.{schema_name}.{make_random(6).lower()}"
+
+    engine = DQEngine(client)
+    engine.save_checks_in_table(TEST_CHECKS[0], table_name, mode="append")
+    checks = engine.load_checks_from_table(table_name, spark=spark)
+    assert checks == TEST_CHECKS[0], "Checks were not loaded correctly after appending."
+
+    engine.save_checks_in_table(TEST_CHECKS[1:], table_name, mode="overwrite")
+    checks = engine.load_checks_from_table(table_name, spark=spark)
+    assert checks == TEST_CHECKS[1:], "Checks were not loaded correctly after overwriting."
--- tests/unit/test_build_rules.py
+++ tests/unit/test_build_rules.py
@@ -464,3 +464,35 @@ def test_build_quality_rules_from_dataframe(spark_local):
     df = DQEngineCore.build_dataframe_from_quality_rules(test_checks, spark=spark_local)
     checks = DQEngineCore.build_quality_rules_from_dataframe(df)
     assert checks == test_checks, "The loaded checks do not match the expected checks."
+
+
+def test_build_quality_rules_from_dataframe_with_run_config(spark_local):
+
+    default_checks = [
+        {
+            "name": "column_is_not_null",
+            "criticality": "error",
+            "check": {"function": "is_not_null", "arguments": {"col_name": "test_col"}},
+        },
+        {
+            "name": "column_is_not_null_or_empty",
+            "criticality": "warn",
+            "check": {"function": "is_not_null_and_not_empty", "arguments": {"col_name": "test_col"}},
+        },
+    ]
+    workflow_checks = [
+        {
+            "name": "column_not_less_than",
+            "criticality": "warn",
+            "check": {"function": "is_not_less_than", "arguments": {"col_name": "test_col", "limit": "5"}},
+        },
+    ]
+    default_checks_df = DQEngineCore.build_dataframe_from_quality_rules(default_checks, spark=spark_local)
+    workflow_checks_df = DQEngineCore.build_dataframe_from_quality_rules(
+        default_checks, run_config_name="workflow_001", spark=spark_local
+    )
+    df = default_checks_df.union(workflow_checks_df)
+    checks = DQEngineCore.build_quality_rules_from_dataframe(df, run_config_name="workflow_001")
+    assert checks == workflow_checks, "The loaded checks do not match the expected workflow checks."
+    checks = DQEngineCore.build_quality_rules_from_dataframe(df)
+    assert checks == default_checks, "The loaded checks do not match the expected default checks."
