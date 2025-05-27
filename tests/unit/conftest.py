335ec91944405d3ac3a0b3f096f87387c70375c3
Marcin Wojtyczka <marcin.wojtyczka@databricks.com>
2025-05-27 13:13:26 +0200

    removed spark remote

--- tests/unit/conftest.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-import pytest
-
-
-@pytest.fixture
-def spark_local():
-    return SparkSession.builder.appName("DQX Test").remote("sc://localhost").getOrCreate()
