ec92f9185e4cfc18b0dc6029e17098e72650b9cb
Greg Hansen <gregory.hansen@databricks.com>
2025-07-15 08:42:42 -0400

    Refactor demo tests

--- tests/e2e/test_run_demos.py
+++ tests/e2e/test_run_demos.py
@@ -1,8 +1,10 @@
+import asyncio
 import logging
 import os
 
 from pathlib import Path
 from databricks.sdk import WorkspaceClient
+from databricks.sdk.service.workspace import ImportFormat
 from databricks.sdk.service.jobs import NotebookTask, SubmitTask, RunLifecycleStateV2State, TerminationTypeType
 
 
@@ -13,23 +15,25 @@ logger = logging.getLogger(__name__)
 
 def test_run_all_demo_notebooks_succeed(make_notebook):
     demo_folder = Path(__file__).parent.parent.parent / "demos"
-    notebook_paths = [path for path in os.listdir(demo_folder) if path.endswith(".py")]
+    notebook_paths = [path for path in os.listdir(demo_folder) if path.endswith((".py", ".ipynb", ".dbc"))]
     ws = WorkspaceClient()
 
-    run_ids = []
-    for notebook_path in notebook_paths:
+    async def get_submit_job_result(notebook_path):
         path = demo_folder / notebook_path
         with open(path, "rb") as f:
-            notebook = make_notebook(content=f)
-            wait_for_run = ws.jobs.submit(
-                tasks=[SubmitTask(task_key="demo_run", notebook_task=NotebookTask(notebook_path=notebook.as_fuse()))]
-            )
-            run_ids.append(wait_for_run.response.run_id)
+            notebook = make_notebook(content=f, format=ImportFormat.AUTO)
+        return await ws.jobs.submit(
+            tasks=[SubmitTask(task_key="demo_run", notebook_task=NotebookTask(notebook_path=notebook.as_fuse()))]
+        )
 
-    for run_id in run_ids:
-        run = ws.jobs.get_run(run_id)
-        if run.status.state == RunLifecycleStateV2State.TERMINATED:
-            run_details = run.status.termination_details
-            assert (
-                run_details.type == TerminationTypeType.SUCCESS
-            ), f"Run ended with status {run_details.type.value}: {run_details.message}"
+    async def validate_submit_job_runs(runs):
+        for completion in asyncio.as_completed(runs):
+            run = await completion
+            if run.status.state == RunLifecycleStateV2State.TERMINATED:
+                run_details = run.status.termination_details
+                assert (
+                    run_details.type == TerminationTypeType.SUCCESS
+                ), f"Run ended with status {run_details.type.value}: {run_details.message}"
+
+    runs = [get_submit_job_result(notebook_path) for notebook_path in notebook_paths]
+    asyncio.run(validate_submit_job_runs(runs))
