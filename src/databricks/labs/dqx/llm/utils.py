e744760a380bcb84f1b755b617f5533470fb3699
karthik-ballullaya-db <karthik.ballullaya@databricks.com>
2025-07-24 11:43:58 +0530

    Add function implementation to the return dictionary and add test case

--- src/databricks/labs/dqx/llm/utils.py
+++ src/databricks/labs/dqx/llm/utils.py
@@ -1,25 +1,34 @@
 import inspect
+from typing import Any
+
 from databricks.labs.dqx.engine import DQEngineCore
 from databricks.labs.dqx.rule import CHECK_FUNC_REGISTRY
 
 
-def get_check_function_defintion() -> list[dict[str, str]]:
+def get_check_function_defintion(custom_check_functions: dict[str, Any] | None = None) -> list[dict[str, str]]:
     """A utility function to get the definition of all check functions.
     This is function is primarily used to generate a prompt for the LLM to generate a check functions.
+
+    :param custom_check_functions: A dictionary of custom check functions.
+        If provided, the function will use the custom check functions to resolve the check function.
+        If not provided, the function will use only the built-in check functions.
 
     Returns:
       list[dict]: A list of dictionaries, each containing the definition of a check function.
     """
     function_docs: list[dict[str, str]] = []
     for name, func_type in CHECK_FUNC_REGISTRY.items():
-        func = DQEngineCore.resolve_check_function(name, fail_on_missing=True)
+        func = DQEngineCore.resolve_check_function(name, custom_check_functions, fail_on_missing=True)
+        sig = inspect.signature(func)
+        doc = inspect.getdoc(func)
         function_docs.append(
             {
                 "name": name,
                 "type": func_type,
-                "doc": inspect.getdoc(func),
-                "signature": inspect.signature(func),
-                "parameters": inspect.signature(func).parameters,
+                "doc": doc or "",
+                "signature": str(sig),
+                "parameters": str(sig.parameters),
+                "implementation": inspect.getsource(func),
             }
         )
     return function_docs
