396f44d453f16e97d34c38c1d31c9841c3e20f0a
Greg Hansen <gregory.hansen@databricks.com>
2025-07-27 08:31:52 -0400

    Update prompts extension

--- tests/e2e/test_run_demos.py
+++ tests/e2e/test_run_demos.py
@@ -151,13 +151,13 @@ def test_run_dqx_dlt_demo(make_notebook, make_pipeline):
 def test_run_dqx_demo_tool(installation_ctx, make_schema, make_notebook, make_job):
     catalog = "main"
     schema = make_schema(catalog_name=catalog).name
-    installation_ctx.prompts.extend(
-        {
+    installation_ctx.replace(
+        extend_prompts={
             r'Provide location for the input data *': '/databricks-datasets/delta-sharing/samples/nyctaxi_2019',
             r'Provide format for the input data (e.g. delta, parquet, csv, json) *': 'delta',
             r'Provide output table in the format `catalog.schema.table` or `schema.table`': f'{catalog}.{schema}.output_table',
             r'Provide quarantined table in the format `catalog.schema.table` or `schema.table`': f'{catalog}.{schema}.quarantine_table',
-        }
+        },
     )
     installation_ctx.workspace_installer.run(installation_ctx.config)
     product_name = installation_ctx.product_info.product_name()
