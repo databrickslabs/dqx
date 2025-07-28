5cb3f8d2ef481159309ff0e2585a77b20cdca8aa
karthik-ballullaya-db <karthik.ballullaya@databricks.com>
2025-07-28 12:43:29 +0530

    Remove test patch to registry

--- tests/unit/test_llm_utils.py
+++ tests/unit/test_llm_utils.py
@@ -1,5 +1,3 @@
-from unittest.mock import patch
-
 import pyspark.sql.functions as F
 
 from databricks.labs.dqx.check_funcs import make_condition, register_rule
@@ -7,7 +5,7 @@ from databricks.labs.dqx.llm.utils import get_check_function_defintion
 
 
 @register_rule("row")
-def dummy_custom_check_function(column: str, suffix: str):
+def dummy_custom_check_function_test(column: str, suffix: str):
     """
     Test the custom check function.
     """
@@ -16,19 +14,19 @@ def dummy_custom_check_function(column: str, suffix: str):
     )
 
 
-@patch.dict(
-    'databricks.labs.dqx.rule.CHECK_FUNC_REGISTRY',
-    [("dummy_custom_check_function", "row")],
-    clear=True,
-)
 def test_get_check_function_defintion():
-    custom_check_functions = {"dummy_custom_check_function": dummy_custom_check_function}
-    result = get_check_function_defintion(custom_check_functions)
+    custom_check_functions = {"dummy_custom_check_function_test": dummy_custom_check_function_test}
+    result = list(
+        filter(
+            lambda x: x['name'] == 'dummy_custom_check_function_test',
+            get_check_function_defintion(custom_check_functions),
+        )
+    )
     assert result[0] == {
-        'name': 'dummy_custom_check_function',
+        'name': 'dummy_custom_check_function_test',
         'type': 'row',
         'doc': 'Test the custom check function.',
         'signature': '(column: str, suffix: str)',
         'parameters': 'OrderedDict([(\'column\', <Parameter "column: str">), (\'suffix\', <Parameter "suffix: str">)])',
-        'implementation': '@register_rule("row")\ndef dummy_custom_check_function(column: str, suffix: str):\n    """\n    Test the custom check function.\n    """\n    return make_condition(\n        F.col(column).endswith(suffix), f"Column {column} ends with {suffix}", f"{column}_ends_with_{suffix}"\n    )\n',
+        'implementation': '@register_rule("row")\ndef dummy_custom_check_function_test(column: str, suffix: str):\n    """\n    Test the custom check function.\n    """\n    return make_condition(\n        F.col(column).endswith(suffix), f"Column {column} ends with {suffix}", f"{column}_ends_with_{suffix}"\n    )\n',
     }
