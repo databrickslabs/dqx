9b956559fea272985591bcab6945476ab425dc2d
Greg Hansen <gregory.hansen@databricks.com>
2025-08-03 09:47:08 -0400

    Use environments with Databricks connect

--- src/databricks/labs/dqx/pii/nlp_engine_config.py
+++ src/databricks/labs/dqx/pii/nlp_engine_config.py
@@ -18,38 +18,3 @@ class NLPEngineConfig(Enum):
         "nlp_engine_name": "spacy",
         "models": [{"lang_code": "en", "model_name": "en_core_web_lg"}],
     }
-
-    STANFORD_DEIDENTIFIER_BASE = {
-        'nlp_engine_name': 'transformers_stanford_deidentifier_base',
-        'models': [
-            {
-                'lang_code': 'en',
-                'model_name': {'spacy': 'en_core_web_sm', 'transformers': 'StanfordAIMI/stanford-deidentifier-base'},
-            }
-        ],
-        'ner_model_configuration': {
-            'labels_to_ignore': ['O'],
-            'aggregation_strategy': 'max',
-            'stride': 16,
-            'alignment_mode': 'expand',
-            'model_to_presidio_entity_mapping': {
-                'PER': 'PERSON',
-                'LOC': 'LOCATION',
-                'ORG': 'ORGANIZATION',
-                'AGE': 'AGE',
-                'ID': 'ID',
-                'EMAIL': 'EMAIL',
-                'PATIENT': 'PERSON',
-                'STAFF': 'PERSON',
-                'HOSP': 'ORGANIZATION',
-                'PATORG': 'ORGANIZATION',
-                'DATE': 'DATE_TIME',
-                'PHONE': 'PHONE_NUMBER',
-                'HCW': 'PERSON',
-                'HOSPITAL': 'LOCATION',
-                'VENDOR': 'ORGANIZATION',
-            },
-            'low_confidence_score_multiplier': 0.4,
-            'low_score_entity_names': ['ID'],
-        },
-    }
--- src/databricks/labs/dqx/pii/pii_detection_funcs.py
+++ src/databricks/labs/dqx/pii/pii_detection_funcs.py
@@ -1,10 +1,10 @@
+import importlib
 import logging
 import json
 import warnings
 
 from collections.abc import Callable
 from presidio_analyzer import AnalyzerEngine
-from presidio_analyzer.nlp_engine import NlpEngineProvider
 
 from pyspark.sql import Column
 from pyspark.sql.functions import concat_ws, lit, pandas_udf, PandasUDFType
@@ -14,11 +14,11 @@ from databricks.labs.dqx.rule import register_rule
 from databricks.labs.dqx.check_funcs import make_condition, _get_normalized_column_and_expr
 from databricks.labs.dqx.pii.nlp_engine_config import NLPEngineConfig
 
-logging.getLogger("presidio-analyzer").setLevel(logging.ERROR)
+logging.getLogger("presidio_analyzer").setLevel(logging.ERROR)
 logger = logging.getLogger(__name__)
 
 _default_nlp_engine_config = NLPEngineConfig.SPACY_SMALL
-_required_modules = ["presidio-analyzer", "spacy"]
+_required_modules = ["presidio_analyzer", "spacy"]
 
 
 @register_rule("row")
@@ -44,13 +44,6 @@ def contains_pii(
     :return: Column object for condition that fails when PII is detected
     """
     warnings.warn(
-        "PII detection checks require installation of the "
-        f"following libraries: {_required_modules}; Creating new SparkSession"
-    )
-    env = DatabricksEnv().withDependencies(_required_modules)
-    DatabricksSession.builder.withEnvironment(env).getOrCreate()
-
-    warnings.warn(
         "PII detection uses user-defined functions which may degrade performance. "
         "Sample or limit large datasets when running PII detection."
     )
@@ -58,8 +51,15 @@ def contains_pii(
     if threshold < 0.0 or threshold > 1.0:
         raise ValueError(f"Provided threshold {threshold} must be between 0.0 and 1.0")
 
-    analyzer = _get_analyzer(nlp_engine_config)
-    entity_detection_udf = _build_detection_udf(analyzer, language, threshold, entities)
+    if not nlp_engine_config:
+        nlp_engine_config = _default_nlp_engine_config
+
+    config_dict = nlp_engine_config if isinstance(nlp_engine_config, dict) else nlp_engine_config.value
+    if not isinstance(config_dict, dict):
+        raise ValueError(f"Invalid type provided for 'nlp_engine_config': {type(nlp_engine_config)}")
+
+    _get_connect_env(config_dict)
+    entity_detection_udf = _build_detection_udf(config_dict, language, threshold, entities)
     col_str_norm, _, col_expr = _get_normalized_column_and_expr(column)
     entity_info = entity_detection_udf(col_expr)
     condition = entity_info.isNotNull()
@@ -68,24 +68,56 @@ def contains_pii(
     return make_condition(condition=condition, message=message, alias=f"{col_str_norm}_contains_pii")
 
 
-def _get_analyzer(nlp_engine_config: NLPEngineConfig | dict | None) -> AnalyzerEngine:
+def _get_connect_env(nlp_engine_config: dict) -> None:
+    """
+    Gets a Databricks Connect environment where PII detection dependencies are pre-loaded on
+    the Spark cluster for use by user-defined functions. Modifies the existing `SparkSession`.
+
+    :param nlp_engine_config: Dictionary configuring the NLP engine used for PII detection
+    """
+    import spacy  # pylint: disable=import-outside-toplevel
+
+    warnings.warn(
+        "PII detection checks require installation of the "
+        f"following libraries: {_required_modules}; Updating SparkSession"
+    )
+
+    modules = _required_modules.copy()
+    for model_name in _get_model_names(nlp_engine_config):
+        spacy.load(model_name)
+        importlib.import_module(model_name)
+        modules.append(model_name)
+
+    env = DatabricksEnv().withDependencies(modules)
+    DatabricksSession.builder.withEnvironment(env).getOrCreate()
+
+
+def _get_model_names(nlp_engine_config: dict) -> list[str]:
+    """
+    Gets a PII detection model name from configuration as `NLPEngineConfig` or Python dictionary.
+
+    :param nlp_engine_config: Dictionary configuring the NLP engine used for PII detection
+    :return: Name of the PII detection model as `str`
+    """
+    if not isinstance(nlp_engine_config, dict):
+        raise ValueError(f"Invalid type provided for 'nlp_engine_config': {type(nlp_engine_config)}")
+
+    return [model["model_name"] for model in nlp_engine_config["models"]]
+
+
+def _get_analyzer(nlp_engine_config: dict) -> AnalyzerEngine:
     """
     Gets an `AnalyzerEngine` for use with PII detection checks.
 
     :param nlp_engine_config: Optional dictionary configuring the NLP engine used for PII detection
     :return: Presidio `AnalyzerEngine`
     """
-    if not nlp_engine_config:
-        nlp_engine_config = _default_nlp_engine_config
-
-    nlp_engine_config_params = (
-        nlp_engine_config.value if isinstance(nlp_engine_config, NLPEngineConfig) else nlp_engine_config
+    from presidio_analyzer.analyzer_engine import (  # pylint: disable=import-outside-toplevel, redefined-outer-name, reimported
+        AnalyzerEngine,
     )
+    from presidio_analyzer.nlp_engine import NlpEngineProvider  # pylint: disable=import-outside-toplevel
 
-    if not isinstance(nlp_engine_config_params, dict):
-        raise TypeError(f"Invalid type for provided 'nlp_engine_config': {type(nlp_engine_config_params)}")
-
-    provider = NlpEngineProvider(nlp_configuration=nlp_engine_config_params)
+    provider = NlpEngineProvider(nlp_configuration=nlp_engine_config)
     nlp_engine = provider.create_engine()
     analyzer = AnalyzerEngine(nlp_engine=nlp_engine)
 
@@ -93,12 +125,12 @@ def _get_analyzer(nlp_engine_config: NLPEngineConfig | dict | None) -> AnalyzerE
 
 
 def _build_detection_udf(
-    analyzer: AnalyzerEngine, language: str, threshold: float, entities: list[str] | None
+    nlp_engine_config: dict, language: str, threshold: float, entities: list[str] | None
 ) -> Callable:
     """
     Builds a UDF with the provided threshold, entities, language, and analyzer.
 
-    :param analyzer: Presidio `AnalyzerEngine` used for named entity detection
+    :param nlp_engine_config: Dictionary configuring the NLP engine used for PII detection
     :param language: Language of the text
     :param threshold: Confidence threshold for named entity detection (0.0 to 1.0)
     :param entities: List of entities to detect
@@ -107,6 +139,7 @@ def _build_detection_udf(
 
     @pandas_udf("string", PandasUDFType.SCALAR)
     def handler(batch):
+        analyzer = _get_analyzer(nlp_engine_config)
         return batch.map(lambda text: _detect_named_entities(text, analyzer, language, threshold, entities))
 
     return handler
