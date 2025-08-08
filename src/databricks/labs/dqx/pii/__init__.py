111a0a7e3d1c04fa722dcbec990575b25b1b0ce5
Greg Hansen <gregory.hansen@databricks.com>
2025-08-08 16:24:32 -0400

    Fmt

--- src/databricks/labs/dqx/pii/__init__.py
+++ src/databricks/labs/dqx/pii/__init__.py
@@ -9,7 +9,7 @@ required_specs = [
 ]
 
 # Check that PII detection modules are installed
-if not all([find_spec(spec) for spec in required_specs]):
+if not all(find_spec(spec) for spec in required_specs):
     raise ImportError(
         "PII detection extras not installed; Install additional "
         "dependencies by running `pip install databricks-labs-dqx[pii]"
