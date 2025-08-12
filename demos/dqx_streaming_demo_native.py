e6bad71826cd9a70b2002848835b8c534b5078df
Marcin Wojtyczka <marcin.wojtyczka@databricks.com>
2025-08-13 00:07:21 +0200

    test

--- demos/dqx_streaming_demo_native.py
+++ demos/dqx_streaming_demo_native.py
@@ -235,8 +235,8 @@ dq_engine.apply_checks_by_metadata_and_save_in_table(
 
 # COMMAND ----------
 
-display(spark.sql(f"SELECT * FROM {silver_table}"))
-display(spark.sql(f"SELECT * FROM {quarantine_table}"))
+#display(spark.sql(f"SELECT * FROM {silver_table}"))
+#display(spark.sql(f"SELECT * FROM {quarantine_table}"))
 
 # COMMAND ----------
 
