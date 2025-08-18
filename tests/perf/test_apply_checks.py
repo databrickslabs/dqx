df30456ec5df23a03a4844bc631956b81a756fa6
Eniwoke Cornelius <cornzyblack@users.noreply.github.com>
2025-08-18 22:38:18 +0100

    tests: add additional tests and refactor duplicate code

--- tests/perf/test_apply_checks.py
+++ tests/perf/test_apply_checks.py
@@ -1,31 +1,81 @@
 from databricks.labs.dqx.engine import DQEngine
 from datetime import datetime
-from databricks.labs.dqx.rule import (
-    ExtraParams,
-)
+from databricks.labs.dqx.rule import ExtraParams, DQRowRule
+import pytest
+from databricks.labs.dqx import check_funcs
+from tests.perf.conftest import ROWS
 
-SCHEMA = "col1: int, col2: int, col3: int, col4: array<int>, col5: date, col6: timestamp, col7: map<string, int>, col8: struct<field1: int>, col9: string"
 RUN_TIME = datetime(2025, 1, 1, 0, 0, 0, 0)
 EXTRA_PARAMS = ExtraParams(run_time=RUN_TIME)
-
-
-def get_checked_df(ws, all_row_checks, generated_df):
-    dq_engine = DQEngine(workspace_client=ws, extra_params=EXTRA_PARAMS)
-    checked_df = dq_engine.apply_checks_by_metadata(generated_df, all_row_checks)
-    return checked_df
+EXPECTED_ROWS = ROWS
 
 
 def test_benchmark_apply_checks_all_row_checks(benchmark, ws, all_row_checks, generated_df):
-    checked_df = benchmark(get_checked_df, ws, all_row_checks, generated_df)
-    assert checked_df.count() == 10_000_000, "Row count after applying checks does not match expected count"
+    dq_engine = DQEngine(workspace_client=ws, extra_params=EXTRA_PARAMS)
+    checked_df = benchmark(dq_engine.apply_checks_by_metadata, generated_df, all_row_checks)
+    actual_count = checked_df.count()
+    assert actual_count == EXPECTED_ROWS
 
 
-# def test_apply_checks_all_row_checks_with_streaming(ws, spark, table_name, all_row_checks, generated_df):
+@pytest.mark.parametrize("column", ["col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8", "col9"])
+def test_benchmark_is_null_or_empty(benchmark, ws, generated_df, column):
+    dq_engine = DQEngine(workspace_client=ws, extra_params=EXTRA_PARAMS)
+    checks = [
+        DQRowRule(
+            name=f"{column}_is_null_or_empty",
+            criticality="warn",
+            check_func=check_funcs.is_not_null_and_not_empty,
+            column=column,
+        ),
+    ]
+    checked = benchmark(dq_engine.apply_checks, generated_df, checks)
+    actual_count = checked.count()
+    assert actual_count == EXPECTED_ROWS
 
-#     dq_engine = DQEngine(workspace_client=ws, extra_params=EXTRA_PARAMS)
-#     row_checks = all_row_checks()
-#     # df.write.format("delta").mode("overwrite").saveAsTable(table_name)
 
-#     streaming_test_df = spark.readStream.table(table_name)
-#     streaming_checked_df = dq_engine.apply_checks_by_metadata(streaming_test_df, row_checks)
-#     assert streaming_checked_df.count() == 10_000_000, "Row count after applying checks does not match expected count"
+@pytest.mark.parametrize("column", ["col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8", "col9"])
+def test_benchmark_is_not_empty(benchmark, ws, generated_df, column):
+    dq_engine = DQEngine(workspace_client=ws, extra_params=EXTRA_PARAMS)
+    checks = [
+        DQRowRule(
+            name=f"{column}_is_not_empty",
+            criticality="warn",
+            check_func=check_funcs.is_not_empty,
+            column=column,
+        ),
+    ]
+    checked = benchmark(dq_engine.apply_checks, generated_df, checks)
+    actual_count = checked.count()
+    assert actual_count == EXPECTED_ROWS
+
+
+@pytest.mark.parametrize("column", ["col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8", "col9"])
+def test_benchmark_is_not_null(benchmark, ws, generated_df, column):
+    dq_engine = DQEngine(workspace_client=ws, extra_params=EXTRA_PARAMS)
+    checks = [
+        DQRowRule(
+            name=f"{column}_is_not_null",
+            criticality="warn",
+            check_func=check_funcs.is_not_null,
+            column=column,
+        ),
+    ]
+    checked = benchmark(dq_engine.apply_checks, generated_df, checks)
+    actual_count = checked.count()
+    assert actual_count == EXPECTED_ROWS
+
+
+@pytest.mark.parametrize("column", ["col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8", "col9"])
+def test_benchmark_is_not_null_and_is_in_list(benchmark, ws, generated_df, column):
+    dq_engine = DQEngine(workspace_client=ws, extra_params=EXTRA_PARAMS)
+    checks = [
+        DQRowRule(
+            name=f"{column}_is_not_null_and_is_in_list",
+            criticality="warn",
+            check_func=check_funcs.is_not_null_and_is_in_list,
+            column=column,
+        ),
+    ]
+    checked = benchmark(dq_engine.apply_checks, generated_df, checks)
+    actual_count = checked.count()
+    assert actual_count == EXPECTED_ROWS
