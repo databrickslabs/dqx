0f104135cd043c5676f13dfc7f4aa60061ef3e02
Greg Hansen <gregory.hansen@databricks.com>
2025-08-20 18:45:29 -0400

    Don't destroy bundle in tests

--- tests/e2e/test_run_demos.py
+++ tests/e2e/test_run_demos.py
@@ -254,7 +254,6 @@ def test_run_dqx_demo_asset_bundle(make_schema, library_ref):
             cwd=path,
         )
         subprocess.run([cli_path, "bundle", "run", "dqx_demo_job"], check=True, capture_output=True, cwd=path)
-        subprocess.run([cli_path, "bundle", "destroy", "--auto-approve"], check=True, capture_output=True, cwd=path)
     except subprocess.CalledProcessError as ex:
         raise AssertionError(ex.stderr) from ex
 
