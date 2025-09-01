306b345dc05eabc617179a58efdc5c9544b1aad8
Marcin Wojtyczka <marcin.wojtyczka@databricks.com>
2025-09-01 15:11:05 +0200

    Update tests/perf/test_apply_checks.py

--- tests/perf/test_apply_checks.py
+++ tests/perf/test_apply_checks.py
@@ -207,7 +207,7 @@ def test_benchmark_is_not_null_and_is_in_list(benchmark, ws, generated_df, colum
 )
 @pytest.mark.benchmark(group="test_benchmark_foreach_is_not_null_and_is_in_list")
 def test_benchmark_foreach_is_not_null_and_is_in_list(benchmark, ws, generated_integer_df):
-    columns, df, n_rows = generated_string_df
+    columns, df, n_rows = generated_integer_df
     dq_engine = DQEngine(workspace_client=ws, extra_params=EXTRA_PARAMS)
     checks = [
         *DQForEachColRule(
