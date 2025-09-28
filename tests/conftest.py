2edab9a198c4638cff82a733bbe06b6f563f2a74
Marcin Wojtyczka <marcin.wojtyczka@databricks.com>
2025-09-28 09:29:41 +0200

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,7 +22,7 @@ from databricks.sdk.service.workspace import ImportFormat
 
 @pytest.fixture
 def debug_env_name():
-    return "ws"  # Specify the name of the debug environment from ~/.databricks/debug-env.json
+    return "ws2"  # Specify the name of the debug environment from ~/.databricks/debug-env.json
 
 
 @pytest.fixture
