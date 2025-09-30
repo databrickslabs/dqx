69c041e31a4400c1ff7c8d315857c3d80f8000ed
Marcin Wojtyczka <marcin.wojtyczka@databricks.com>
2025-09-30 13:01:38 +0200

    Update src/databricks/labs/dqx/geo/check_funcs.py

--- src/databricks/labs/dqx/geo/check_funcs.py
+++ src/databricks/labs/dqx/geo/check_funcs.py
@@ -254,7 +254,7 @@ def is_multilinestring(column: str | Column) -> Column:
     return make_condition(
         condition,
         F.concat_ws("", F.lit("value `"), col_expr.cast("string"), F.lit(condition_str)),
-        f"{col_str_norm}_is_not_a_multilinestring",
+        f"{col_str_norm}_is_not_multilinestring",
     )
 
 
