c5d09d1708dccfb8f9d88214f4ee7815b16fadbf
Marcin Wojtyczka <marcin.wojtyczka@databricks.com>
2025-10-30 11:32:54 +0100

    debug

--- tests/e2e/test_run_demos.py
+++ tests/e2e/test_run_demos.py
@@ -327,6 +327,12 @@ def validate_run_status(run: Run, client: WorkspaceClient) -> None:
     task = run.tasks[0]
     termination_details = run.status.termination_details
 
+    run_output = client.jobs.get_run_output(task.run_id)
+    logger.info("Run output:")
+    logger.info(run_output.as_dict())
     assert (
         termination_details.type == TerminationTypeType.SUCCESS
-    ), f"Run of '{task.task_key}' failed with message: {client.jobs.get_run_output(task.run_id).error}"
+    ), (f"Run of '{task.task_key}' "
+        f"failed with message: {run_output.error}, "
+        f"error trace: {run_output.error_trace}, "
+    )
