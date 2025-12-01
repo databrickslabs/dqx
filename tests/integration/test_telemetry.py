e58550ade6572c60ce0b19d1b72e34eb6f103b27
Marcin Wojtyczka <marcin.wojtyczka@databricks.com>
2025-12-01 17:15:19 +0100

    added integration tests

--- tests/integration/test_telemetry.py
+++ tests/integration/test_telemetry.py
@@ -44,7 +44,22 @@ def test_count_tables_from_table_based_dataframe(ws, spark, make_schema, make_ra
     assert count == 1, f"Expected 1 table, but found {count}"
 
 
-def test_count_multiple_tables_from_table_based_dataframe(ws, spark, make_schema, make_random):
+def test_count_tables_from_aggregated_table_based_dataframe(ws, spark, make_schema, make_random):
+    catalog_name = TEST_CATALOG
+    schema_name = make_schema(catalog_name=catalog_name).name
+    table_name = f"{catalog_name}.{schema_name}.{make_random(10).lower()}"
+
+    input_schema = T.StructType([T.StructField("id", T.IntegerType())])
+    df = spark.createDataFrame([[1]], schema=input_schema).groupBy("id").count()
+    df.write.format("delta").saveAsTable(table_name)
+    test_df = spark.table(table_name)
+
+    count = count_tables_in_spark_plan(test_df)
+
+    assert count == 1, f"Expected 1 table, but found {count}"
+
+
+def test_count_multiple_joined_tables_from_table_based_dataframe(ws, spark, make_schema, make_random):
     catalog_name = TEST_CATALOG
     schema_name = make_schema(catalog_name=catalog_name).name
     table_name1 = f"{catalog_name}.{schema_name}.{make_random(10).lower()}"
@@ -57,6 +72,25 @@ def test_count_multiple_tables_from_table_based_dataframe(ws, spark, make_schema
     test_df1 = spark.table(table_name1)
     test_df2 = spark.table(table_name2)
     test_df = test_df1.join(test_df2, on="id", how="inner")
+
+    count = count_tables_in_spark_plan(test_df)
+
+    assert count == 2, f"Expected 2 tables, but found {count}"
+
+
+def test_count_multiple_unioned_tables_from_table_based_dataframe(ws, spark, make_schema, make_random):
+    catalog_name = TEST_CATALOG
+    schema_name = make_schema(catalog_name=catalog_name).name
+    table_name1 = f"{catalog_name}.{schema_name}.{make_random(10).lower()}"
+    table_name2 = f"{catalog_name}.{schema_name}.{make_random(10).lower()}"
+
+    input_schema = T.StructType([T.StructField("id", T.IntegerType())])
+    df = spark.createDataFrame([[1]], schema=input_schema)
+    df.write.format("delta").saveAsTable(table_name1)
+    df.write.format("delta").saveAsTable(table_name2)
+    test_df1 = spark.table(table_name1)
+    test_df2 = spark.table(table_name2)
+    test_df = test_df1.union(test_df2)
 
     count = count_tables_in_spark_plan(test_df)
 
